3e1ca35d2514b2d92b0fe8867dba5e930251721e
Liel Ran <liel.ran@hibob.io>
2016-04-10 22:07:32 +0300

    add search box (aws link)

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,16 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="productLink">
+          <input  type="text" defaultValue="Amazon link!" />
+        </div>
+
         <div className="notice">second deploy</div>
       </div>
     );
